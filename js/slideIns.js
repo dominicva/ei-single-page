e69854525f268d0dc1718e4749237d336c965ead
Dominic <dominicvana@gmail.com>
2021-01-18 07:34:44 -0500

    Mission animation delays tweaked

--- js/slideIns.js
+++ js/slideIns.js
@@ -4,47 +4,49 @@ export const missionTextEl = document.querySelector('.mission__text');
 
 const missionStatementOptions = {
   root: null,
-  rootMargin: '0px 0px 0px 0px',
-  threshold: 0.5,
+  rootMargin: '0px',
+  threshold: 0,
 };
 
 const missionTitleOptions = {
   root: null,
-  rootMargin: '0px 0px 0px 0px',
+  rootMargin: '0px 0px 100px 0px',
   threshold: 0,
 };
 
 const missionTextOptions = {
   root: null,
-  rootMargin: '0px 0px 50px 0px',
+  rootMargin: '0px',
   threshold: 0,
-};
-
-// list of observer options
-const options = {
-  root: null,
-  rootMargin: '0px 0px 0px 0px',
-  threshold: 0.5,
 };
 
 // instantiate a new Intersection Observer
 export const missionStatementObserver = new IntersectionObserver(entries => {
   entries.forEach(entry => {
+    console.log(entry);
+
     entry.target.classList.add('slide-in--up');
   });
 }, missionStatementOptions);
 
 export const missionTitleObserver = new IntersectionObserver(entries => {
   entries.forEach(entry => {
-    entry.target.style.animationDelay = '1s';
-    entry.target.classList.add('slide-in--up');
+    console.log(entry);
+    console.log('Title', entry.intersectionRatio);
+
+    if (entry.isIntersecting) {
+      entry.target.style.animationDelay = '0.5s';
+      entry.target.classList.add('slide-in--up');
+    }
   });
 }, missionTitleOptions);
 
 export const missionTextObserver = new IntersectionObserver(entries => {
   entries.forEach(entry => {
-    if (entry.intersectionRatio > 0) {
-      entry.target.style.animationDelay = '1.5s';
+    console.log(entry);
+    console.log('Text', entry.intersectionRatio);
+    if (entry.isIntersecting) {
+      entry.target.style.animationDelay = '0.25s';
       entry.target.classList.add('slide-in--up');
     }
   });
