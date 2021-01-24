0de08ed4c1f261844559bfc1c55ba3e6a71ccdf8
Dominic <dominicvana@gmail.com>
2021-01-24 13:49:12 -0500

    Project hover animation for content title

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ import {
   projectImageParallaxer,
   projectImageParallaxerObserver,
 } from './js/projectsSlideIns';
+import { gridItem, projectImage, projectContentTitle } from './js/projectsGrid';
 
 import('./js/header').then(header => {
   header.toggleMenuHandler();
@@ -26,3 +27,17 @@ typeWriterObserver.observe(projectsHeaderTitle);
 
 // **NB to be converted to querySelectorAll when more images added**
 projectImageParallaxerObserver.observe(projectImageParallaxer);
+
+// project grid image hover handler
+gridItem.addEventListener('mouseenter', () => {
+  if (window.innerWidth >= 1024) {
+    projectImage.style.opacity = '0';
+    projectContentTitle.style.bottom = '50%';
+  }
+});
+gridItem.addEventListener('mouseleave', () => {
+  if (window.innerWidth >= 1024) {
+    projectImage.style.opacity = '1';
+    projectContentTitle.style.bottom = '0%';
+  }
+});
---
+++ js/projectsGrid.js
@@ -0,0 +1,8 @@
+export const gridItem = document.querySelector(
+  '.project-grid__grid-item-container'
+);
+export const projectImage = document.querySelector('.project-grid__image');
+
+export const projectContentTitle = document.querySelector(
+  '.project-grid__content__title'
+);
