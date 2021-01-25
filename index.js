a36a735cb6bd1330c76029eb265647a724228fba
Dominic <dominicvana@gmail.com>
2021-01-25 13:24:31 -0500

    Refactor: project hover handlers to module

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ import {
   projectContentHeadline,
   projectContentBody,
   projectContentCta,
+  projectMouseEnterHandler,
+  projectMouseLeaveHandler,
 } from './js/projectsGrid';
 
 import('./js/header').then(header => {
@@ -36,70 +38,5 @@ typeWriterObserver.observe(projectsHeaderTitle);
 // **NB to be converted to querySelectorAll when more images added**
 projectImageParallaxerObserver.observe(projectImageParallaxer);
 
-// project grid image hover handler
-gridItemContainer.addEventListener('mouseenter', () => {
-  if (window.innerWidth >= 1024) {
-    // BORDER
-    gridItem.style.borderColor = '#ffb300';
-
-    // IMAGE
-    projectImage.style.opacity = '0';
-
-    // SET PRE-ANIMATION STYLE PROPERTY VALUES
-    projectContentHeadline.style.display = 'block';
-    projectContentHeadline.style.position = 'absolute';
-    projectContentHeadline.style.top = '70%';
-    projectContentHeadline.style.paddingLeft = '24px';
-
-    projectContentBody.style.display = 'block';
-    projectContentBody.style.position = 'absolute';
-    projectContentBody.style.bottom = '0%';
-    projectContentBody.style.paddingLeft = '24px';
-
-    projectContentCta.style.display = 'flex';
-    projectContentCta.style.position = 'absolute';
-    projectContentCta.style.bottom = '-100%';
-    projectContentCta.style.paddingLeft = '24px';
-    projectContentCta.style.paddingRight = '24px';
-    projectContentCta.style.width = '100%';
-
-    setTimeout(() => {
-      // CONTENT HEADLINE
-      projectContentHeadline.style.opacity = '1';
-      projectContentHeadline.style.top = '5%';
-
-      // CONTENT TITLE
-      projectContentTitle.style.bottom = '60%';
-
-      // CONTENT BODY
-      projectContentBody.style.opacity = '1';
-      projectContentBody.style.bottom = '42%';
-
-      // CONTENT CTA
-      projectContentCta.style.opacity = '1';
-      projectContentCta.style.bottom = '5%';
-    }, 20);
-  }
-});
-
-gridItemContainer.addEventListener('mouseleave', () => {
-  if (window.innerWidth >= 1024) {
-    // BORDER
-    gridItem.style.borderColor = '';
-
-    // IMAGE
-    projectImage.style.opacity = '1';
-
-    // CONTENT HEADLINE
-    projectContentHeadline.style = '';
-
-    // CONTENT TITLE
-    projectContentTitle.style.bottom = '0';
-
-    // CONTENT BODY
-    projectContentBody.style = '';
-
-    // CONTENT CTA
-    projectContentCta.style = '';
-  }
-});
+gridItemContainer.addEventListener('mouseenter', projectMouseEnterHandler);
+gridItemContainer.addEventListener('mouseleave', projectMouseLeaveHandler);
--- js/projectsGrid.js
+++ js/projectsGrid.js
@@ -22,26 +22,73 @@ export const projectContentCta = document.querySelector(
   '.project-grid__content__cta'
 );
 
-/* 
-display | all
-position | all
-top | headline
-padding-left | all
-padding-right | cta 
-width | cta
-opacity | all
-bottom | body, cta
-*/
-
 const setElCssProps = (element, props) => {
   for (const prop in props) {
     element.style[prop] = props[prop];
   }
 };
 
-const projectHoverHandler = () => {
+export const projectMouseEnterHandler = () => {
   if (window.innerWidth >= 1024) {
     setElCssProps(gridItem, { borderColor: '#ffb300' });
     setElCssProps(projectImage, { opacity: '0' });
+    // HEADLINE
+    setElCssProps(projectContentHeadline, {
+      display: 'block',
+      position: 'absolute',
+      top: '70%',
+      paddingLeft: '24px',
+    });
+    // BODY
+    setElCssProps(projectContentBody, {
+      display: 'block',
+      position: 'absolute',
+      bottom: '0%',
+      paddingLeft: '24px',
+    });
+    // CTA
+    setElCssProps(projectContentCta, {
+      display: 'flex',
+      position: 'absolute',
+      bottom: '-100%',
+      paddingLeft: '24px',
+      paddingRight: '24px',
+      width: '100%',
+    });
+
+    const delay = 20;
+    setTimeout(() => {
+      setElCssProps(projectContentHeadline, {
+        opacity: '1',
+        top: '5%',
+      });
+      setElCssProps(projectContentTitle, {
+        bottom: '60%',
+      });
+      setElCssProps(projectContentBody, {
+        opacity: '1',
+        bottom: '42%',
+      });
+      setElCssProps(projectContentCta, {
+        opacity: '1',
+        bottom: '5%',
+      });
+    }, delay);
+  }
+};
+
+export const projectMouseLeaveHandler = () => {
+  if (window.innerWidth >= 1024) {
+    gridItem.style.borderColor = '';
+    projectImage.style.opacity = '1';
+    projectContentHeadline.style = '';
+    projectContentTitle.style.bottom = '0';
+    projectContentBody.style = '';
+    projectContentCta.style = '';
+    // setElCssProps(gridItem, { borderColor: '' });
+    // setElCssProps(projectImage, { opacity: '1' });
+    // setElCssProps(projectContentHeadline, { opacity: '0%', top: '70%' });
+    // setElCssProps(projectContentTitle, { bottom: '0%' });
+    // setElCssProps(projectContentBody, { bottom: '%' });
   }
 };
