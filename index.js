b1c5e2f2af859757f8154432281f8ec46293df59
Dominic <dominicvana@gmail.com>
2021-01-25 08:19:49 -0500

    Project grid hover animation

--- index.js
+++ index.js
@@ -11,7 +11,15 @@ import {
   projectImageParallaxer,
   projectImageParallaxerObserver,
 } from './js/projectsSlideIns';
-import { gridItem, projectImage, projectContentTitle } from './js/projectsGrid';
+import {
+  gridItemContainer,
+  gridItem,
+  projectImage,
+  projectContentTitle,
+  projectContentHeadline,
+  projectContentBody,
+  projectContentCta,
+} from './js/projectsGrid';
 
 import('./js/header').then(header => {
   header.toggleMenuHandler();
@@ -29,15 +37,69 @@ typeWriterObserver.observe(projectsHeaderTitle);
 projectImageParallaxerObserver.observe(projectImageParallaxer);
 
 // project grid image hover handler
-gridItem.addEventListener('mouseenter', () => {
+gridItemContainer.addEventListener('mouseenter', () => {
   if (window.innerWidth >= 1024) {
+    // BORDER
+    gridItem.style.borderColor = '#ffb300';
+
+    // IMAGE
     projectImage.style.opacity = '0';
-    projectContentTitle.style.bottom = '50%';
+
+    // SET PRE-ANIMATION STYLE PROPERTY VALUES
+    projectContentHeadline.style.display = 'block';
+    projectContentHeadline.style.position = 'absolute';
+    projectContentHeadline.style.top = '70%';
+    projectContentHeadline.style.paddingLeft = '24px';
+
+    projectContentBody.style.display = 'block';
+    projectContentBody.style.position = 'absolute';
+    projectContentBody.style.bottom = '0%';
+    projectContentBody.style.paddingLeft = '24px';
+
+    projectContentCta.style.display = 'flex';
+    projectContentCta.style.position = 'absolute';
+    projectContentCta.style.bottom = '-100%';
+    projectContentCta.style.paddingLeft = '24px';
+    projectContentCta.style.paddingRight = '24px';
+    projectContentCta.style.width = '100%';
+
+    setTimeout(() => {
+      // CONTENT HEADLINE
+      projectContentHeadline.style.opacity = '1';
+      projectContentHeadline.style.top = '5%';
+
+      // CONTENT TITLE
+      projectContentTitle.style.bottom = '60%';
+
+      // CONTENT BODY
+      projectContentBody.style.opacity = '1';
+      projectContentBody.style.bottom = '42%';
+
+      // CONTENT CTA
+      projectContentCta.style.opacity = '1';
+      projectContentCta.style.bottom = '5%';
+    }, 20);
   }
 });
-gridItem.addEventListener('mouseleave', () => {
+
+gridItemContainer.addEventListener('mouseleave', () => {
   if (window.innerWidth >= 1024) {
+    // BORDER
+    gridItem.style.borderColor = '';
+
+    // IMAGE
     projectImage.style.opacity = '1';
-    projectContentTitle.style.bottom = '0%';
+
+    // CONTENT HEADLINE
+    projectContentHeadline.style = '';
+
+    // CONTENT TITLE
+    projectContentTitle.style.bottom = '0';
+
+    // CONTENT BODY
+    projectContentBody.style = '';
+
+    // CONTENT CTA
+    projectContentCta.style = '';
   }
 });
--- js/projectsGrid.js
+++ js/projectsGrid.js
@@ -1,8 +1,23 @@
-export const gridItem = document.querySelector(
+export const gridItemContainer = document.querySelector(
   '.project-grid__grid-item-container'
 );
+
+export const gridItem = document.querySelector('.project-grid__grid__item');
+
 export const projectImage = document.querySelector('.project-grid__image');
 
 export const projectContentTitle = document.querySelector(
   '.project-grid__content__title'
+);
+
+export const projectContentHeadline = document.querySelector(
+  '.project-grid__content__headline'
+);
+
+export const projectContentBody = document.querySelector(
+  '.project-grid__content__body'
+);
+
+export const projectContentCta = document.querySelector(
+  '.project-grid__content__cta'
 );
