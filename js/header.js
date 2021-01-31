3de124f2fb151e55faf17388bea8fc04d4f0c2fe
Dominic <dominicvana@gmail.com>
2021-01-31 16:49:36 -0500

    Responsive header tweaks

--- js/header.js
+++ js/header.js
@@ -12,17 +12,3 @@ export const toggleMenuHandler = () => {
     }
   });
 };
-
-// export const headerNavColorHandler = () => {
-//   const headerNav = document.querySelector('.main-header.wrapper');
-
-//   const quoteContainerHeight = document.querySelector('.quote.container')
-//     .clientHeight;
-//   const scrollPosition = window.scrollY;
-
-//   if (scrollPosition >= quoteContainerHeight) {
-//     headerNav.classList.add('light-bg');
-//   } else {
-//     headerNav.classList.remove('light-bg');
-//   }
-// };
